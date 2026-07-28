e095d04af09deee341267adc0f50158f1d01029c
ajimcse <mdajimsorkarcse40@gmail.com>
2026-07-28 22:46:33 +0600

    ES6 let const

---
+++ let-const.js
@@ -0,0 +1,22 @@
+let many= 50
+// console.log(many)
+// let many2 = 50;
+const many2 = 50;
+console.log(many2)
+// many2= 60;
+// console.log(many2)
+
+const student ={
+    name:'ajim',
+    age:25
+}
+// console.log(student)
+student.name='sorkar';
+// console.log(student)
+let sum =0;
+
+for(let i = 0;  i < 10; i++){
+   const num = i;
+   sum = sum + num;
+}
+console.log(sum)
