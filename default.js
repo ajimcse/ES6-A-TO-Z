9c5a0466a76722c96ecdc97ec8ae04d57f350ede
ajimcse <mdajimsorkarcse40@gmail.com>
2026-07-29 22:04:21 +0600

    default and template

--- default.js
+++ default.js
@@ -1,1 +1,8 @@
- 
+ function add(num1, num2=9){
+    const result = num1 + num2;
+    console.log(num1, num2, result)
+    return result;
+ }
+//  const sum  = add(5, 7)
+ const sum  = add(5)
+ console.log(sum)
---
+++ template.js
@@ -0,0 +1,5 @@
+const first  = 'ajim'; 
+const last= 'sorkar';
+ 
+const fullname= first +" " + last ;
+console.log(fullname)
