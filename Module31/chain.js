b7ef0a261f510e93c4ee9c5b84d43f4955b086b3
ajimcse <mdajimsorkarcse40@gmail.com>
2026-08-02 21:10:00 +0600

    chainjs

---
+++ Module31/chain.js
@@ -0,0 +1,24 @@
+const data = [{ id: 1, name: 'abul', adderss: 'PPI' }]
+// console.log(data)
+// console.log(data[0].adderss);
+const products = {
+    count: 5000,
+    data: [
+        { id: 1, name: 'lwnovo laptop', price: 50000 },
+        { id: 1, name: 'macbook', price: 150000 }
+    ]
+}
+// console.log(products.data[1].price)
+
+const user ={
+    id:8400,
+    name:'ajim',
+    adderss:{
+        pabna:{
+            first:'boroitala',
+            second:'botala',
+            third:'meril baipass'
+        }
+    }
+}
+console.log(user.adderss.pabna.second)
