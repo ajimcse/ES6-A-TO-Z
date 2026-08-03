524eb4527d2dce8032557140baab06804e6b4c1a
ajimcse <mdajimsorkarcse40@gmail.com>
2026-08-03 22:54:16 +0600

    class

---
+++ Module31/class.js
@@ -0,0 +1,18 @@
+// class Product{
+//  country ='Bangladesh'
+ 
+// }
+// const lenovo =new Product();
+// console.log(lenovo)
+
+class Teacher {
+   constructor(name, subject){
+      this.name =name
+      this.subject =subject
+   }
+   lecture(){
+      console.log('sir is teaching math')
+   }
+}
+const ajim = new Teacher('ajimsorka', 'math');
+console.log(ajim)
