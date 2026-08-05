53f4bf364f416d5099780a99e896a8d94661729a
ajimcse <mdajimsorkarcse40@gmail.com>
2026-08-05 21:44:51 +0600

    problem 2

--- Module31/practice-problem.js
+++ Module31/practice-problem.js
@@ -8,8 +8,50 @@
 // }
 // console.log(even)
 
-const numbers = [1, 3, 5, 7, 9]
+// const numbers = [1, 3, 5, 7, 9]
 
-const  result = numbers.map(num => num + 1);
-console.log(result)
+// const  result = numbers.map(num => num + 1);
+// console.log(result)
 
+const numbers = [33, 50, 79, 78, 90, 101, 30];
+
+// const result = numbers.filter(num => num % 10 ===0);
+// console.log(result)
+const reslut = numbers.find(num => num % 10 === 0)
+// console.log(reslut)
+
+//  practice-problem2========================================
+
+const instructor = [
+    { name: 'ajim', age: 28, position: 'senior' },
+    { name: 'sorkar', age: 30, position: 'senior' },
+    { name: 'kalam', age: 25, position: 'junior' },
+    { name: 'sabul', age: 32, position: 'senior' }
+]
+
+// for(const arr of instructor){
+//    if(arr.position ==='senior'){
+//     console.log(arr)
+//    }
+// }
+const seniorInstuctor = instructor.filter(person => person.position === 'senior');
+// console.log(seniorInstuctor)
+
+
+const people = [
+    { name: 'ajim', age: 28, position: 'senior' },
+    { name: 'sorkar', age: 30, position: 'senior' },
+    { name: 'kalam', age: 25, position: 'junior' },
+    { name: 'sabul', age: 32, position: 'senior' }
+];
+// let sum = 0
+// for (const person of people) {
+//       sum = sum + person.age
+
+// }
+// console.log(sum)
+const sum = people.reduce((total, person) => {
+    return total + person.age;
+}, 0);
+
+console.log(sum);
