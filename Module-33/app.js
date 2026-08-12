66bb7bac6253cb2d6bbbf5e9e952a81bc665b1d9
ajimcse <mdajimsorkarcse40@gmail.com>
2026-08-12 21:13:12 +0600

    loadData theke createElemet

--- Module-33/app.js
+++ Module-33/app.js
@@ -1,5 +1,13 @@
 function dataLoad2(){
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(res => res.json())
     .then(data =>console.log(data) )
+}
+function dataLoadUser(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data =>displayUsers(data) )
+} 
+function displayUsers(value){
+    console.log(value)
 }
---
+++ Module-33/index.js
@@ -0,0 +1,14 @@
+function loadUser2() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => loadUserData2(data))
+}
+function loadUserData2(users) {
+    const ul =document.getElementById('user-list')
+     for(const user of users){
+       console.log(user.username);
+        const li = document.createElement('li');
+        li.innerText= user.name;
+        ul.appendChild(li)
+     }
+}
