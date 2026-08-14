58ce35340998136e70d8747df0bb9e3a61f18ae4
ajimcse <mdajimsorkarcse40@gmail.com>
2026-08-14 22:36:02 +0600

    callstack and asyns

---
+++ module-34/asyns.js
@@ -0,0 +1,23 @@
+// 🧠 শুধু এই কথাটা মনে রাখো:
+
+// Asynchronous = সময় লাগা কোনো কাজের জন্য JavaScript পুরো program-কে আটকে রাখে না; কাজটা চলতে থাকাকালীন অন্য code execute করতে পারে।
+
+// console.log(1)
+// console.log(2)
+// // console.log(3)
+// // doSomething()
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+
+// function doSomething(){
+//     console.log(3)
+// }
+
+ console.log("start")
+  setTimeout(() =>{
+   console.log('Hello')
+
+  } ,2000)
+  console.log('End')
---
+++ module-34/globalAndCallStack.js
@@ -0,0 +1,10 @@
+function first() {
+    console.log("First");
+    second();
+}
+
+function second() {
+    console.log("Second");
+}
+
+first();
