685a8e4b644f121fb5ac7108df7ab6a319872563
ajimcse <mdajimsorkarcse40@gmail.com>
2026-08-15 23:38:56 +0600

    asyncAwait

---
+++ module-34/asyncAwait.js
@@ -0,0 +1,9 @@
+async function loadUsers() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+    const data = await response.json();
+
+    console.log(data);
+}
+
+loadUsers();
