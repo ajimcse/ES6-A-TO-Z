186c6d50883a91b158f79b866ff3021fbe31e60b
ajimcse <mdajimsorkarcse40@gmail.com>
2026-08-16 22:56:01 +0600

    .then() async and async/await & async arrow function

--- module-34/asyncAwait.js
+++ module-34/asyncAwait.js
@@ -1,15 +1,15 @@
-const myLoader =() =>{
-    return new Promise((resolve, reject) =>{
-        const success = true
-        if(success){
-            console.log('data')
-        }
-        else{
-            console.log('not data')
-        }
-    })
-}
-myLoader()
+// const myLoader =() =>{
+//     return new Promise((resolve, reject) =>{
+//         const success = true
+//         if(success){
+//             console.log('data')
+//         }
+//         else{
+//             console.log('not data')
+//         }
+//     })
+// }
+// myLoader()
 
 // async function loadUsers() {
 //     const response = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -20,3 +20,23 @@ myLoader()
 // }
 
 // loadUsers();
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then(res => res.json())
+// .then(data => console.log(data))
+// .catch(err => console.log(err))
+
+
+//  async function loadData() {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await res.json()
+//     console.log(data)
+// }
+// loadData()
+
+const  dataLoad = async () =>{
+    const data2 =  await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await data2.json()
+    console.log(users)
+}
+dataLoad()
