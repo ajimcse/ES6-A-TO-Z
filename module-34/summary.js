1417d7454d6cb3c4646d5bf0dfbaf2d8b465a138
ajimcse <mdajimsorkarcse40@gmail.com>
2026-08-18 20:41:24 +0600

    practice summary

---
+++ module-34/summary.js
@@ -0,0 +1,33 @@
+function loadDataUser() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(arr))
+}
+
+async function loadDataUserAsync() {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json();
+    console.log(data)
+}
+
+const loadArrowData = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json();
+    console.log(data)
+}
+
+const loadDataTryAndCatch = async () => {
+    try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+        const data = await res.json();
+       console.log(data)
+    }
+    catch(error) {
+        console.log('data not foned')
+    }
+
+}
+ 
